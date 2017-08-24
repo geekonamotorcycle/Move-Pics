78a1494cf821141efc1fde8b2288b9fc882a780a
Joshua Porrata <Joshua.porrata@gmail.com>
2017-08-24 17:22:32 -0400

    Add files via upload
    funtions/Copyright.ps1

---
+++ Functions/Copyright.ps1
@@ -0,0 +1,10 @@
+Function Copyright {
+    Write-Host "`n***********************************************" -BackgroundColor Black -ForegroundColor DarkGreen
+    Write-Host "***Copyright 2017, Joshua Porrata**************" -BackgroundColor Black -ForegroundColor DarkGreen
+    Write-Host "***This program is not free for business use***" -BackgroundColor Black -ForegroundColor DarkGreen
+    Write-Host "***Contact me at localbeautytampabay@gmail.com*" -BackgroundColor Black -ForegroundColor DarkGreen
+    Write-Host "***for a cheap business license****************" -BackgroundColor Black -ForegroundColor DarkGreen
+    Write-Host "***Donations are wholeheartedly accepted ******" -BackgroundColor Black -ForegroundColor Red
+    Write-Host "***accepted @ www.paypal.me/lbtpa**************" -BackgroundColor Black -ForegroundColor Red
+    Write-Host "***********************************************`n" -BackgroundColor Black -ForegroundColor DarkGreen
+}
